08529a2ce063a42812a6a9e9ad60953e1c9af1ea
GithoupCMD <jonhreader79@gmail.com>
2026-05-26 17:24:08 +0300

    Added 3 functions: 1. Fred 2. Fgreen 3. Fblue
    They receive a string and return the same string with escape codes (\033) and the standart ANSI color codes for red, green, and blue, respectively

---
+++ main.go
@@ -0,0 +1,13 @@
+package tineecolor
+
+func Fred(s string) string {
+	return "\033[31m" + s + "\033[0m"
+}
+
+func Fgreen(s string) string {
+	return "\033[31m" + s + "\033[0m"
+}
+
+func Fblue(s string) string {
+	return "\033[31m" + s + "\033[0m"
+}
